f69261c06c3bb2e0915cdba8e6c5e07ecc572995
Mohamed Esmail <dev.mohamed.esmail@gmail.com>
2026-05-22 11:31:20 +0300

    fix setting service error

--- src/settings/settings.module.ts
+++ src/settings/settings.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SettingsService } from './settings.service';
 import { SettingsController } from './settings.controller';
+import { CloudinaryModule } from '../cloudinary/cloudinary.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule, CloudinaryModule],
   providers: [SettingsService],
   controllers: [SettingsController]
 })
